676c6f7a8b09f073b0ad9a111ad7d56f3343d261
Aswathi Krishnan <66994935+aswathik98@users.noreply.github.com>
2020-07-15 20:38:26 +0530

    Add files via upload

--- src/model/Blog.java
+++ src/model/Blog.java
@@ -1,2 +1,38 @@
 package model;
 // Type your code
+
+import java.time.LocalDate;
+public class Blog{
+	
+	private String title;
+	private int description;
+	private LocalDate postedOn;
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public int getDescription() {
+		return description;
+	}
+	public void setDescription(int description) {
+		this.description = description;
+	}
+	public LocalDate getPostedOn() {
+		return postedOn;
+	}
+	public void setPostedOn(LocalDate postedOn) {
+		this.postedOn = postedOn;
+	}
+	public Blog(String title, int description, LocalDate postedOn) {
+		
+		this.title = title;
+		this.description = description;
+		this.postedOn = postedOn;
+	}
+	
+	
+	
+}
